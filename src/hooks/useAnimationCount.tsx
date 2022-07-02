11e9a9902ce00085e72800f0a5bea7a34627d873
ChangHyun2 <jchangh2@loplat.com>
2022-07-02 19:39:06 +0900

    feat: 카운터 애니메이션 ease-in 효과 적용

--- src/hooks/useAnimationCount.tsx
+++ src/hooks/useAnimationCount.tsx
@@ -1,26 +1,44 @@
 import { useCallback, useRef, useState } from 'react'
 
+const MAX_FPX = 60
+
 export default function useAnimationCount(
   start: number,
   end: number,
   milliSec: number,
 ): [number, () => void] {
   const [count, setCount] = useState(start)
-  const startAt = useRef<number | null>(null)
+  const startAtRef = useRef<number | null>(null)
+  const fps = useRef(MAX_FPX)
 
-  const increase = useCallback(
-    (timestamp: number) => {
-      if (!startAt.current) {
-        startAt.current = timestamp
+  const draw = useCallback(
+    (timestamp: number, prevTimeStamp: number) => {
+      // 첫 프레임일 때
+      if (!startAtRef.current) {
+        startAtRef.current = timestamp
       }
 
-      const progress = timestamp - startAt.current
+      // x축:t , y축:progress
+      const spf = 1 / fps.current
+      const startAt = startAtRef.current
 
-      if (progress < milliSec) {
-        const currentRatio = progress / milliSec
-        setCount(Math.floor(currentRatio * (end - start) + start))
+      const t = timestamp - startAt
+      const progress = t / milliSec
 
-        requestAnimationFrame(increase)
+      // 타원에서 y축 offset 4
+      fps.current =
+        Math.abs(MAX_FPX * (Math.sqrt(1 - (progress - 1) ** 2) - 1)) + 5
+
+      if ((timestamp - prevTimeStamp) / 1000 < spf) {
+        requestAnimationFrame((timestamp) => draw(timestamp, prevTimeStamp))
+        return
+      }
+
+      if (t < milliSec) {
+        setCount(Math.floor(progress * (end - start) + start))
+
+        const currentTimeStamp = timestamp
+        requestAnimationFrame((timestamp) => draw(timestamp, currentTimeStamp))
       } else {
         setCount(end)
       }
@@ -29,8 +47,8 @@ export default function useAnimationCount(
   )
 
   const requestAnimation = useCallback(
-    () => requestAnimationFrame(increase),
-    [increase],
+    () => requestAnimationFrame((timestamp) => draw(timestamp, timestamp)),
+    [draw],
   )
 
   return [count, requestAnimation]
